88f0ddb4433d2ce1864f1381e9d44a3f25939ee9
Angel Calvo <polloverdetm@gmail.com>
2014-05-05 19:41:00 +0200

    Problem 03

---
+++ 03/c03.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+#######################################
+# Tuenti Challange 4
+# Problem 3
+#
+# Angel Calvo
+#######################################
+
+# Main block
+if __FILE__ == $0
+  cases = ARGF.readline.to_i
+  cases.times do |i|
+	x, y = ARGF.readline.split.map {|x| x.to_i}
+	gamble = Math.sqrt(x*x + y*y).round(2)
+	puts "#{gamble}"
+  end
+end
